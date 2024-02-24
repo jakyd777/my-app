365c28a76729bf1214bbfdbbae980a0d55aca22e
Vladimir Jakubik <vladimir.jakubik77@gmail.com>
2024-02-24 16:36:29 +0100

    Add startin code to components App, Footer, Header and Note

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
 
 function App() {
-    return <h1>Hello Vladimir!</h1>
+    return <div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>
 }
 
 export default App;
---
+++ src/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>Title of the Note</h1>
+        <p>Content of the note.</p>
+    </div>
+}
+
+export default Note;
